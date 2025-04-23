9b7bb750a746ce07d154182b1a90b9fda06133ba
Shresth Jain <shresth.jn19@gmail.com>
2025-04-24 00:14:49 +0530

    initial commit

---
+++ backend/main.py
@@ -0,0 +1,38 @@
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import os
+from apps.calculator.route import router as calculator_router
+
+SERVER_URL = os.getenv("SERVER_URL", "localhost")
+PORT = int(os.getenv("PORT", 8900))  # ← cast to int
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+async def root():
+    return {"message": "server is running OP"}
+
+
+app.include_router(calculator_router, prefix="/calculate", tags=["calculate"])
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app", host=SERVER_URL, port=PORT, reload=(os.getenv("ENV") == "dev")
+    )
